79b9b4a03f7853d92f72cb177afc655f742935fe
ktrogue39@gmail.com <kmr108@pitt.edu>
2016-01-22 09:47:50 -0500

    added to Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,9 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Test!");
+		System.out.println("Test 2!");
+		System.out.println("Test 3!");
+		
     }
 
 }
